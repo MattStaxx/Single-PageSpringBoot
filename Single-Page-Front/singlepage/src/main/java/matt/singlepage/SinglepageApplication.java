f408c4e3247df9122dc41d865d35daac9b3e2d4f
Matt Stallman <matthew.stallman@hcl.com>
2021-05-10 14:05:21 -0500

    Initial commit

---
+++ Single-Page-Front/singlepage/src/main/java/matt/singlepage/SinglepageApplication.java
@@ -0,0 +1,13 @@
+package matt.singlepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinglepageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinglepageApplication.class, args);
+	}
+
+}
